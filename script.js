fa8156831db9484580bfe9a1507030c8d1f199ab
akiunnico <141770165+akiunnico@users.noreply.github.com>
2024-12-01 21:08:09 +0100

    Finish game

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 let numeroCasillas = 16;
+let encendido = false;
+let estilo = "black";
 
-const contenedor = document.querySelector(".container");
+const botones = document.querySelectorAll(".botonera > button");
+
+botones.forEach((e) => e.addEventListener("click", activaBoton));
+
+const off = document.querySelector(".botonera > p");
+
+const lienzo = document.querySelector(".lienzo");
+
+
+function pintaCuadricula(numeroCasillas){
+  lienzo.textContent = "";
+  let anchoCasilla = 600 / numeroCasillas;
+  for (i=0; i<Math.pow(numeroCasillas, 2); i++){
+    let div = document.createElement("div");
+    
+      div.addEventListener("mouseout", pinta);
+
+    div.style.width = (`${anchoCasilla}px`);
+    div.style.height = (`${anchoCasilla}px`);
+    // div.textContent = i;
+    lienzo.appendChild(div);
+  }
+}
 
 pintaCuadricula(numeroCasillas);
 
-const boton = document.querySelector("button");
+const boton = document.querySelector(".boton");
 
 boton.addEventListener("click", pideNumeroCasillas)
 
@@ -17,19 +41,54 @@ function pideNumeroCasillas(){
   } 
 }
 
-function pintaCuadricula(numeroCasillas){
-  contenedor.textContent = "";
-  let anchoCasilla = 960 / numeroCasillas;
-  for (i=0; i<Math.pow(numeroCasillas, 2); i++){
-    let div = document.createElement("div");
-    
-    div.addEventListener("mouseover", () => div.style.backgroundColor = "black");
 
-    div.style.width = (`${anchoCasilla}px`);
-    div.style.height = (`${anchoCasilla}px`);
-    // div.textContent = i;
-    contenedor.appendChild(div);
+lienzo.addEventListener("click", enciendeApaga);
+
+function enciendeApaga(){
+  encendido = !encendido;
+  if(encendido) {
+    off.classList.add("encendido");
+    off.textContent = "Encendido";
+  }else{
+    off.classList.remove("encendido");
+    off.classList.add("apagado");
+    off.textContent = "Apagado";
   }
+  
+  
 }
 
+function pinta(){
+  if(encendido){
 
+    // botones.forEach((e)=>e.removeAttribute("class"));
+
+    switch(estilo){
+     
+      case "random":
+        this.style.backgroundColor = `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`;
+        break;
+      case "white":
+        this.style.backgroundColor = "white";
+        break;
+        default: 
+        this.style.backgroundColor = "black";
+    }
+  }
+ 
+}
+
+function setEstilo(seleccion){
+  estilo = seleccion;
+}
+function clean(){
+  const casillas = document.querySelectorAll(".lienzo>div");
+  casillas.forEach(casilla => {casilla.style.backgroundColor = "white";
+    
+  });
+}
+
+function activaBoton(){
+  botones.forEach((e)=> e.removeAttribute("class"));
+  this.classList.add("active");
+}
